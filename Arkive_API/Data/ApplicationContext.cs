faed2ae826241ad175a4431f20705414e649ff68
Sabelli <sabellinho@gmail.com>
2026-05-09 13:48:04 -0300

    chore: configurando base do projeto

---
+++ Arkive_API/Data/ApplicationContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Arkive_API.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
